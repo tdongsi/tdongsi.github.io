a9f7634d19c60d36a481f8024f8646e1da2f0ab2
tdongsi <dongsi.tuecuong@gmail.com>
2016-03-21 01:26:56 -0700

    Blog: Runner template.

---
+++ source/_posts/SQL/Temp.java
@@ -0,0 +1,30 @@
+    private CodeStrategy codeHandler;
+	private TestStrategy testHandler;
+    private JdbcConnection connection;
+        
+    @Override
+    public void runScript(String filePath) throws IOException, SQLException {
+        SoftAssert sAssert = new SoftAssert();
+		
+		// Read in the SQL script
+		String content = SqlTestUtility.readFile(filePath);
+		
+		// Remove comments
+		String sqlCode = TestBlockUtility.removeComments(content);
+		
+		Matcher m = TestBlockUtility.testBlockRegex.matcher(sqlCode);
+		int startIndex = 0;
+		while (m.find()) {
+			
+			String currentSql = sqlCode.substring(startIndex, m.start());
+			if ( currentSql.trim().length() > 0 )
+				codeHandler.runSqlCode(currentSql, connection);;
+			
+			testHandler.runTest( m.group(), connection, sAssert );
+			
+			startIndex = m.end();
+		}
+		
+		codeHandler.runSqlCode(sqlCode.substring(startIndex), connection );
+		sAssert.assertAll();
+    }
