a8ab4ed4b6fb340c26b01d378fa08cfbf14177ff
tdongsi <dongsi.tuecuong@gmail.com>
2017-07-12 02:27:52 -0700

    Octopress: Separate variables.

--- plugins/youtube.rb
+++ plugins/youtube.rb
@@ -1,15 +1,15 @@
 module Jekyll
   class Youtube < Liquid::Tag
-    @width = 640
-    @height = 390
 
     def initialize(name, id, tokens)
       super
       @id = id
+      @width = 640
+      @height = 480
     end
 
     def render(context)
-      %(<iframe width="640" height="480" src="http://www.youtube.com/embed/#{@id}" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>)
+      %(<iframe width='#{@width}' height='#{@height}' src='http://www.youtube.com/embed/#{@id}' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>)
     end
   end
 end
